56fdc33833c52a9d956a1a7f8de298dba8766ecc
Troy Willmot <yort@yortondotnet.com>
2019-07-25 20:03:35 +1200

    Add NetStandard 2.0 project. Remove WPSL. Update nuspec and copyright dates etc.

--- nuget-push.cmd
+++ nuget-push.cmd
@@ -1,2 +1,2 @@
-.nuget\nuget.exe push Yort.Otp.1.0.5.0.nupkg
+.nuget\nuget.exe push Yort.Otp.1.0.6.0.nupkg
 pause
---
+++ src/Yort.Otp.NetStandard20/AssemblyInfo.cs
@@ -0,0 +1,12 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Yort.Otp.NetStandard20")]
+[assembly: AssemblyCulture("")]
+[assembly: AssemblyDescription("One Time Password library for .Net Standarrd 2.0.")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
